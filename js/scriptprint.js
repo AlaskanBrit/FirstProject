7e790b69108c7b9a67f43d7dc34bfb213bc6420c
AlaskanBrit <41019004+AlaskanBrit@users.noreply.github.com>
2018-07-12 13:34:07 +0100

    Added in time rather than seconds input
    Changes to js code to add in time functionality and recalculate the pace time.

--- js/scriptprint.js
+++ js/scriptprint.js
@@ -18,7 +18,7 @@ function multiply(){
 	var result = Number(num1)* Number(num2);
 	document.getElementById("demo").innerHTML = result;
 }
-
+9
 function divide(){
 	var num1 = document.getElementById("firstnumber").value;
 	var num2 = document.getElementById("secondnumber").value;
@@ -28,7 +28,9 @@ function divide(){
 */
 
 function pacecalculate(){
-	var targettime = document.getElementById("targettime").value;
+	var targettime = returntime();
+	document.getElementById("testtext").innerHTML = targettime;
+	//var targettime = document.getElementById("targettime").value;
 	//var racedistance = document.getElementById("racedistance").value;
 
 	var divider = 0;
@@ -37,15 +39,27 @@ function pacecalculate(){
 
 	if (km) {
 		divider = 1000;
-		document.getElementById("pace text").innerHTML = 'Pace (s/km)';
+		document.getElementById("pace text").innerHTML = 'Pace (mm:ss/km)';
 	}
 	else{
 		divider = 1608;
-		document.getElementById("pace text").innerHTML = 'Pace (s/mile)';
+		document.getElementById("pace text").innerHTML = 'Pace (mm:ss/mile)';
 	}
 	//document.getElementById("demo").innerHTML = km;
-	var calculatedpace = Number(targettime)*60*Number(divider)/passdistance();
-	document.getElementById("pace value").innerHTML = calculatedpace;
+	var calculatedpace = Number(targettime)*Number(divider)/passdistance();
+	
+	
+	var mins = Math.floor(calculatedpace/60);
+	if(mins < 10){
+	mins = `0${mins}`;
+	}
+	var seconds = calculatedpace%60;
+	//seconds = math.round(Number(seconds));
+	if(seconds < 10){
+	seconds = `0${seconds}`;
+	}
+	var timestring = `${mins}:${seconds}`;
+	document.getElementById("pace value").innerHTML = timestring;
 }
 
 function passdistance(){
@@ -53,4 +67,18 @@ function passdistance(){
 	var distanceval = e.options[e.selectedIndex].value;
 	//document.getElementById("testtext").innerHTML = distanceval;
 	return Number(distanceval);
+}
+
+function returntime(){
+	var racetime = document.getElementById("targettime2").value;
+		document.getElementById("testtext").innerHTML = racetime;
+	var timearray = racetime.split(":");
+	
+	var gethours = timearray[0];
+	var getmins  = timearray[1];
+	var getsecs  = timearray[2];
+	
+	var totalseconds = 3600*Number(gethours) + 60*Number(getmins) + 1*Number(getsecs);
+	document.getElementById("testtext").innerHTML = totalseconds;
+	return totalseconds;
 }
